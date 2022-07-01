29c94a6ddb441a94240bf88e07fa61b82493a635
Bintang <bintangprka@gmail.com>
2022-07-02 01:32:45 +0800

    navbar

--- index.php
+++ index.php
@@ -1,7 +1,29 @@
 	<html>
-	<head><title>Login</title></head>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+		<title>Login</title>
+	</head>
+
 	<body>
 
+	<div class="navbar">
+		<div class="navbarname">
+			<a><span style="color:#1D8348">KAK SU</span><span style="color:#2E86C1">NASI KERABU</span></a>  
+		</div>
+
+		<div class="navbarbutton">
+			<ul>
+				<li><a href="#home">Menu</a></li>
+				<li><a href="#news">About us</a></li>
+				<li><a href="#contact">Contact Us</a></li>
+				<li style="column-width:60px"><a href="#cart"><span class="material-symbols-outlined">shopping_cart</span></a></li>
+			</ul>
+		</div>
+	</div>
+	
+		
+	
 		<b>Welcome! Please log in before proceeding</b>
 	    <br><br>
 		<form method="post" action="check_login.php">
