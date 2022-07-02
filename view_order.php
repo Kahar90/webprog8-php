e317994f222eb56a839b4d03283424427cf31659
Rayhan Rafi <rayhanrafi246@gmail.com>
2022-07-02 17:10:43 +0800

    q

--- view_order.php
+++ view_order.php
@@ -14,9 +14,10 @@ $result = mysqli_query($conn, $sql);
 ?>
 <head>
     <link rel="stylesheet" href="css/cart.css">
+    <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-</head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  </head>
 <?php
 if (mysqli_num_rows($result) > 0){ ?>
 
@@ -46,18 +47,50 @@ if (mysqli_num_rows($result) > 0){ ?>
 
 ?>
 
+<div class="navbar">
+        <div class="navbarname">
+            <a href="index.php"><span style="color:#1D8348">KAK SU</span><span style="color:#2E86C1">NASI KERABU</span></a>  
+        </div>
+
+        <div class="navbarbutton">
+            <ul>
+                <li><a href="#home">Menu</a></li>
+                <li><a href="#news">About us</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+                <li style="column-width:60px"><a href="view_order.php"><span class="material-symbols-outlined">shopping_cart</span></a></li>
+            </ul>
+        </div>
+</div>
+
 <div class="shopping-cart">
+  <!-- Title -->
   <div class="title">
-    Shopping Cart
+    CART
   </div>
-  
+ 
+  <!-- Product #1 -->
   <div class="item">
-    <tr>
-        <td>Item name</td>
-        <td>Item Quantity</td>
-        <td>Action</td>
-    </tr>
-    
+ 
+    <div class="image">
+      <img src="img/nasikerabu.jpeg" width="80" length="150"/>
+    </div>
+ 
+    <div class="description">
+      <span>NASI GURIH</span>
+      <span>White</span>
+    </div>
+ 
+    <div class="quantity">
+      <span>Quantity</span>
+      <span>2</span>
+    </div>
+ 
+    <div class="total-price">$549</div>
+
+    <div class="delete-button"><a><span class="material-symbols-outlined">
+    delete
+    </span></a>
+    </div>
   </div>
  
 </div>
