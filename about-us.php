f5829674be4dc08d20c59ffcfaa8980080368199
Rayhan Rafi <rayhanrafi246@gmail.com>
2022-07-02 20:27:48 +0800

    w

---
+++ about-us.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about us</title>
+
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/about-us.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="navbarname">
+            <a href="index.php"><span style="color:#1D8348">KAK SU</span><span style="color:#2E86C1">NASI KERABU</span></a>  
+        </div>
+
+        <div class="navbarbutton">
+            <ul>
+                <li><a href="main.php">Menu</a></li>
+                <li><a href="#news">About us</a></li>
+                <li><a href="#contact">Contact Us</a></li>
+                <li style="column-width:60px"><a href="view_order.php"><span class="material-symbols-outlined">shopping_cart</span></a></li>
+            </ul>
+        </div>
+    </div>
+
+
+    <div class="we-are-block">
+        <div id="about-us-section">
+            <div class="about-us-image">
+                    <img src="https://digitalupgrade.com/images/lobbyimage_1.jpg" width="808" height="458" alt="Lobby Image">
+            </div>
+            <div class="about-us-info">
+                <h2>Sedap Giler</h2>
+                <p>Digital Upgrade, located in Evansville, IN, makes it easy for businesses to create and manage their digital presence and logistics, meaning we can make your business more efficient and profitable! We believe in our community and want to help our local business economy grow and expand. We believe in our clients, and want to assist in their digital success. The D-UP team grades ourselves on outcomes, not on sales.</p>
+            </div>
+        </div>
+        <div id="history-section">
+            <div class="history-image">
+                <img src="https://digitalupgrade.com/images/building_pic.jpg" width="951" height="471" alt="Building Pic">
+            </div>
+            <div class="history-info">
+            <h2>Best Do!</h2>
+            <p>In 2018, technology companies, Desktop Doctors & Digital Upgrade found a new place to call home, at 816 N. 9th Ave. Since the takeover of the 118-year-old building (formally Stippler Tool & Supply and Crown Chair Company), there have been many renovations completed to preserve this local piece of Evansville, IN history.</p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+</html>
