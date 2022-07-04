f40b05a67adecfb232b2c2ad72688267da4a6a2d
Rayhan Rafi <rayhanrafi246@gmail.com>
2022-07-05 02:24:20 +0800

    1

--- order_list.php
+++ order_list.php
@@ -1,28 +1,59 @@
-<?php
-session_start(); // Start up your PHP Session
- 
-require('config.php');//read up on php includes https://www.w3schools.com/php/php_includes.asp
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/cart.css">
+    <link rel="stylesheet" href="css/navbar.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbarname">
+            <a href="index.php"><span style="color:#1D8348">KAK SU</span><span style="color:#2E86C1">NASI KERABU</span></a>  
+        </div>
 
-$sql = "SELECT credentials.name, credentials.address,credentials.phone,usercart.itemname, usercart.itemqtty\n"
+        <div class="navbarbutton">
+            <ul>
+                <li><a href="main.php">Menu</a></li>
+                <li><a href="about-us-admin.php">About us</a></li>
+                <li><a href="contact-us-admin.php">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
 
-    . "FROM credentials\n"
-
-    . "JOIN usercart ON credentials.username=usercart.user;";
+    <div class="shopping-cart">
+        <!-- Title -->
+        <div class="title">
+            LIST OF ORDERED ITEM
+        </div>   
     
-    $result = mysqli_query($conn, $sql);
+        <form method="POST" name="deleteorder" action="deleteorder.php">
+                
+            <!-- Product #1 -->
+            <div class="item">
+                <div class="description">
+                    <input type="text" name="custName" value="Rhavy">
+                </div>
 
-?>
-<?php while($rows = mysqli_fetch_assoc($result)) { ?>
-    <tr>
-        <td><?php echo $rows['name']; ?></td>
-        <td><?php echo $rows['address']; ?></td>
-        <td><?php echo $rows['phone']; ?></td>
-        <td><?php echo $rows['itemname']; ?></td>
-        <td><?php echo $rows['itemqtty']; ?></td>        
-    </tr>
-    <br>
+                <div class="description">
+                    <input type="text" name="custAddress" value="Rhavy">
+                </div>
 
-<?php } ?>
+                <div class="description">
+                    <input type="number" name="phone" value="01139333882">
+                </div>
 
-
-   
+                <div class="description">
+                    <input type="text" name="itemName" value="sotong">
+                </div>
+                    
+                <div class="quantity">
+                    <input type="number" name="quantity" value="4">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
